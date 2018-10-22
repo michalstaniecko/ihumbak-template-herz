824466c011bc951dd9229248577d4b3923e00607
Michal Staniecko <michal.staniecko@gmail.com>
2018-10-22 15:36:26 +0200

    init

---
+++ wp-content/themes/herz-studio/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ * Created by PhpStorm.
+ * User: Michal Staniecko
+ * Date: 22.10.18
+ * Time: 15:29
+ */
---
+++ wp-content/themes/herz-studio/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ * Created by PhpStorm.
+ * User: Michal Staniecko
+ * Date: 22.10.18
+ * Time: 15:29
+ */
+
+echo 'herz studio';
