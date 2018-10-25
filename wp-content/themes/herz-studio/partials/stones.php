006cefbc6375e6a1b391efbbf7bcde42287f658d
Michal Staniecko <michal.staniecko@gmail.com>
2018-10-25 16:52:56 +0200

    parallas - stone images

--- wp-content/themes/herz-studio/partials/stones.php
+++ wp-content/themes/herz-studio/partials/stones.php
@@ -1,5 +1,12 @@
 <div class="c-stones">
-  <div class="c-stones__item c-stones__item--big" style="top: 900px;" data-bottom-top="top: 900px;" data-top-bottom="top:600px;" ></div>
-  <div class="c-stones__item c-stones__item--medium" style="top: 1000px; left:60px" data-bottom-top="top: 1000px;" data-top-bottom="top:450px;" ></div>
-  <div class="c-stones__item c-stones__item--small" style="top: 1100px; left:120px" data-bottom-top="top: 1100px;" data-top-bottom="top:300px;" ></div>
+  <div class="c-stones__item c-stones__item--big" style="top: 900px;" data-bottom-top="top: 900px;" data-top-bottom="top:600px;" >
+    <img src="<?= HERZ_IMG ?>/stones/big-1.png" />
+  </div>
+  <div class="c-stones__item c-stones__item--medium" style="top: 1000px; left:180px" data-bottom-top="top: 1000px;" data-top-bottom="top:450px;" >
+    <img src="<?= HERZ_IMG ?>/stones/medium-1.png" />
+  </div>
+  <div class="c-stones__item c-stones__item--small" style="top: 1100px; left:280px" data-bottom-top="top: 1100px;" data-top-bottom="top:300px;" >
+    <img src="<?= HERZ_IMG ?>/stones/small-1.png" />
+
+  </div>
 </div>
