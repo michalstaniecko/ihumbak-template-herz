252de69d76a0bb91a856ad1aa3b0e4f3f4bdd425
Michal Staniecko <michal.staniecko@gmail.com>
2018-12-17 12:27:28 +0100

    google analytics small fix

--- wp-content/themes/herz-studio/inc/analytics.php
+++ wp-content/themes/herz-studio/inc/analytics.php
@@ -10,20 +10,23 @@
 add_action( 'wp_head', 'google_analytics' );
 
 function google_analytics() {
-  ?>
+  if ( ! is_super_admin() ):
+    ?>
 
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131135493-1"></script>
-  <script>
-		window.dataLayer = window.dataLayer || [];
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131135493-1"></script>
+    <script>
+			window.dataLayer = window.dataLayer || [];
 
-		function gtag() {
-			dataLayer.push(arguments);
-		}
+			function gtag() {
+				dataLayer.push(arguments);
+			}
 
-		gtag('js', new Date());
+			gtag('js', new Date());
 
-		gtag('config', 'UA-131135493-1');
-  </script>
+			gtag('config', 'UA-131135493-1');
+    </script>
   <?php
+
+  endif;
 }
